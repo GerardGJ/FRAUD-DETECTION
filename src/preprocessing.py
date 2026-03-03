68c3032c28c1679d2e486d7fffe06e6a062343ab
Gerard Gomez Jorba <gerard.castefa@gmail.com>
2026-03-03 23:15:09 +0100

    started writting the deployment code for reading the data and preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,123 @@
+# This script is in charge of preprocessing the data
+import polars as pl
+import pandas as pd
+import joblib
+from sklearn.preprocessing import LabelEncoder
+CATEGORICAL_VARIABLES = ["transaction_type", "payment_mode", "device_type", "device_location"]
+
+
+
+class Preprocesser():
+
+    def previousActionsGenerator(self, 
+                                 df: pl.DataFrame
+        ) -> pl.DataFrame: 
+        """
+        Generation of feature engineering variables based on past actions.
+        The features fenerated are:
+            - previous_fraud : Boolean variable that tell if user previously commited faud
+            - previous_fraud_num : Numeric variable that tells how many times was comited fraud previously
+                                    by the user
+            - previous_transactions : Boolean variable that shows if a user previouly made a transaction
+            - previous_transactions_num : Numeric variable that shows how many transaction the user 
+                                            made before
+        
+        Parameters
+        ----------
+            df:pd.DataFrame 
+        
+        Returns
+        -------
+            df:pd.DataFrame
+        """
+
+        df = (
+            df
+            .with_columns(pl.col("transaction_id").str.replace("T","").alias("transaction_id_number"))
+            .cast({"transaction_id_number": pl.Int16})
+            .sort("transaction_id_number")
+
+        )
+
+        df2 = df.select(["transaction_id_number","user_id","fraud_label"])
+
+        df_join = (
+            df
+            .join(df2, 
+                on='user_id',
+                how="inner",
+                suffix='_r')
+            .filter(
+                pl.col('transaction_id_number') > pl.col("transaction_id_number_r")
+            )
+        )
+
+        df_agg =df_join.group_by("user_id","transaction_id_number").agg(
+            fraud_any=pl.col('fraud_label').any().cast(pl.Int8),
+            fraud_number=pl.col("fraud_label").sum(),
+            transaction_any=1,
+            transaction_number=pl.col("fraud_label").count(),
+        )
+
+        df = (
+            df
+            .join(
+                df_agg,
+                on=['user_id','transaction_id_number'],
+                how='left'
+            )
+            .fill_null(0)
+            .drop('transaction_id_number')
+        )
+
+        return df
+
+    def getFeaturnEncodingModles(self, 
+                                 categorical_columns: list[str]
+        ) -> dict[str,LabelEncoder]:
+
+        dict_le = dict()
+        for col in categorical_columns:
+            path = f"utils_models/{col}_LE.joblib"
+            le = joblib.load(path)
+
+            dict_le[col] = le
+
+        return dict_le
+         
+    def featrueEncoding(self, 
+                        dict_le: dict[str,LabelEncoder], 
+                        df: pd.DataFrame, 
+                        categorical_columns: list[str]
+    ) -> pd.DataFrame:
+        
+        for col in categorical_columns:
+            print(f"Encoding column: {col}")
+            encoded_col = dict_le[col].transform(df[col])
+
+            df[col] = encoded_col
+
+        return df
+
+    def transform(self, 
+                  df:pd.DataFrame
+        ) -> pd.DataFrame:
+        """
+        This method is in charge of preprocessing the data
+        """
+
+        df_polars = pl.from_pandas(df)
+        
+        # The first step in the preprocessing step is the 
+        # Feature Engineering
+        df_fe_polars = self.previousActionsGenerator(df_polars)
+        df_fe = df_fe_polars.to_pandas()
+
+        # After this we will encode the different features
+        # First we get the label encoders
+        dict_le = self.getFeaturnEncodingModles(CATEGORICAL_VARIABLES)
+        df_le = self.featrueEncoding(dict_le,df_fe,CATEGORICAL_VARIABLES)
+
+        return df_le
+
+
---
+++ src/readData.py
@@ -0,0 +1,39 @@
+# The main objective of this code is to the data and read it
+# TODO: Implement testing (The dataframe is correctly read)
+# TODO: Also impement another data read for when data given through streamlit or API
+
+import pandas as pd
+import os
+
+class DataReader():
+    """
+    This class reads all the data
+    """
+
+    def read_data(self, path:str) -> tuple[pd.DataFrame, str] :
+        """
+        The main objective of this data set is to read the provided data set
+        and return a pandas dataframe
+
+        Parameters
+        ----------
+        path : str
+            This is the path to the csv file:
+
+        Returns
+        -------
+        dataframe : pd.DataFrame
+            This is the dataframe read from the path provided
+        error : str
+            In case of an error this string has the error, otherwise
+            it is an empty string
+        """
+        dataframe: pd.DataFrame = pd.DataFrame()
+        error: str = ""
+
+        try:
+            dataframe = pd.read_csv(path)
+        except Exception as e:
+            error = f'Function failed, error {e}'
+        finally:
+            return dataframe,error
